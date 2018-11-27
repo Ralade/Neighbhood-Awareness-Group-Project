773c0127b5bc8ba84c91394cf5414e86f69e1eb6
Kun Huang <kkun.huang@gmail.com>
2018-11-26 23:08:38 -0500

    reporting crime view with cancel button

--- neighborhoodAwareness/Controllers/ReportCrimeViewController.swift
+++ neighborhoodAwareness/Controllers/ReportCrimeViewController.swift
@@ -10,13 +10,23 @@ import UIKit
 
 class ReportCrimeViewController: UIViewController {
 
+    
+    @IBOutlet weak var reportCrimeBox: UITextView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
     
-
+    @IBAction func reportCrime(_ sender: UIButton) {
+        
+    }
+    
+    @IBAction func cancelReport(_ sender: UIButton) {
+        dismiss(animated: true, completion: nil)
+    }
+    
     /*
     // MARK: - Navigation
 
