6532b927dceb1ce1ee3f1b6434c2c169bfae6d75
Kun Huang <kkun.huang@gmail.com>
2018-11-27 23:33:03 -0500

    tableview to record user reported crime

---
+++ neighborhoodAwareness/View/ReportCrimeCell.swift
@@ -0,0 +1,26 @@
+//
+//  ReportCrimeCell.swift
+//  neighborhoodAwareness
+//
+//  Created by Kun Huang on 11/27/18.
+//  Copyright © 2018 Hein Soe. All rights reserved.
+//
+
+import UIKit
+
+class ReportCrimeCell: UITableViewCell {
+
+    @IBOutlet weak var crimeDescriptionLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
